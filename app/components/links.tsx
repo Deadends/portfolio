d55cf8e038884d1fcd58864b36d6468a49255dcb
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-09-26 21:42:17 +0530

    Update links.tsx

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -6,7 +6,7 @@ export function LinkPreviewDemo() {
   return (
     <div className="flex justify-center items-center h-[30rem] flex-col px-4 pt-0 pb-22 bg-black ">
       <p className="relative z-10  bg-black text-center text-2xl md:text-2xl py-7  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  font-sans font-bold">
-        Check my recent projects</p>
+        Hover over my recent projects</p>
 
         <p className=" pt-7 relative z-10  bg-black text-center text-1xl md:text-1xl py-4  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  font-sans font-bold">
         Full Scale Industrial Project  
