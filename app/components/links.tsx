96421a51af256d526cefc09015a95f65d7e4fe88
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-09-26 22:41:54 +0530

    Update links.tsx

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -16,7 +16,7 @@ export function LinkPreviewDemo() {
         <p className="relative z-10  bg-black text-center text-1xl md:text-1xl py-4  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  font-sans font-bold">
         E-wallet webpage  
         <LinkPreview url="https://miracle-pages-373873.framer.app/" className="font-bold p-2 dark:text-blue-500  text-blue-500 ">
-          - Vault X
+          - vault X
         </LinkPreview>{" "}
       </p>
     </div>
